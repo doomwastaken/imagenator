57d7e747da74d255520374423ca198dfa26c6292
Artem Muradyan <muradyanartem2@gmail.com>
2022-04-28 18:12:36 +0300

    rework vk teams wrapper (#10)
    Co-authored-by: Artem Muradyan <a.muradyan@corp.mail.ru>

--- imagenator/bot.py
+++ imagenator/bot.py
@@ -1,31 +1,33 @@
 import json
+import typing
 
 import requests
 
 
+def encode_message(message: str) -> str:
+    return message.replace(r"-", r"\-").replace(r"_", r"\_").replace(r"*", r"\*")
+
+
 class Bot:
+    """VK Teams api wrapper"""
+
     def __init__(self, token: str) -> None:
-        self.token = token
-        self.base_api_url = "https://api.internal.myteam.mail.ru/bot/v1"
-        self.send_text_url = f"{self.base_api_url}/messages/sendText"
+        self.token: str = token
+        self.base_api_url: str = "https://api.internal.myteam.mail.ru/bot/v1"
+        self.send_text_url: str = f"{self.base_api_url}/messages/sendText"
 
-    @staticmethod
-    def encode_message(message):
-        return message.replace(r"-", r"\-").replace(r"_", r"\_").replace(r"*", r"\*")
-
-    def send(self, to, message):
-        params = {
+    def send(self, to: str, message: str) -> None:
+        """Send message to VK teams chat"""
+        params: dict = {
             "token": self.token,
             "chatId": to,
-            "text": self.encode_message(message),
+            "text": encode_message(message),
             "parseMode": "MarkdownV2",
         }
-        r = requests.get(self.send_text_url, params=params)
+        r: requests.Response = requests.get(self.send_text_url, params=params)
         if r.status_code != 200:
-            print(f"Запрос в Myteam завершился с ошибкой {r.status_code}")
+            print(f"Request was failed: {r.status_code}")
 
-        resp = r.json()
+        resp: typing.Any = r.json()
         if not resp.get("ok", False):
-            print(
-                f"Не удалось отправить сообщение {message} {to}" f"{json.dumps(resp)}"
-            )
+            print(f"Cannot send message: {message} {to}" f"{json.dumps(resp)}")
